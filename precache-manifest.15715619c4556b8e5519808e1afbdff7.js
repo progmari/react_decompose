7620416fe98d8137533e7c9bb58a37e71342e1b2
Maria Yasevina <maria.yasevina@gmail.com>
2021-05-17 17:57:41 +0300

    Updates

---
+++ precache-manifest.15715619c4556b8e5519808e1afbdff7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3391606030d2d3ac62e8d962b614709d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "93c72585bda763a0717c",
+    "url": "./static/css/main.5d64665c.chunk.css"
+  },
+  {
+    "revision": "a3fa78d2a47eeb043805",
+    "url": "./static/js/2.ac9e10eb.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.ac9e10eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "93c72585bda763a0717c",
+    "url": "./static/js/main.115cbe05.chunk.js"
+  },
+  {
+    "revision": "8b3c3b96386eee1ea33a",
+    "url": "./static/js/runtime-main.b672bc02.js"
+  }
+]);
